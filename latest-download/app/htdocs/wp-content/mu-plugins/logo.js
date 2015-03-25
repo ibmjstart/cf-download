b67041a3f3b9b373cf3a9c80b3947acd25f94392
Miguel Clement <mfclemen@us.ibm.com>
2015-03-25 17:02:24 -0400

    removed downloads

--- latest-download/app/htdocs/wp-content/mu-plugins/logo.js
+++
@@ -1,6 +0,0 @@
-
-window.onload = function() {	
-	jQuery('.site-info > a').html("<a href='http://wordpress.org/'>Proudly powered by WordPress</a><a href='www.bluemix.net'> on IBM Bluemix</a>");
-};
-	
-
